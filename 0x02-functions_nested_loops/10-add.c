1f55f01a17e27286819ffbe4937d4a7a721002d5
blaireKE <kahorablaire54@gmail.com>
2023-07-20 04:55:37 -0700

    all tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two integers and returns the result.
+ * Return: integer value
+ * @n1: the first number to be computed
+ * @n2: the swcond number tobe computed
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,12 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The characters to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _putchar - writes the caharacter c to the stdout
+ * @c: The character to print
+ * Return: 0 (success)
  */
- 
-int _putchar (char c)
+
+int _putchar(char c)
 {
-	return (write (1,&c, 1));
+	return (write(1, &c, 1));
 }
