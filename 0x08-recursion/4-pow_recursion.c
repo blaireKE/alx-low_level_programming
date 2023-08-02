da0af29c2cd860fe2c111dc5b4870eb6917278ba
blaireKE <kahorablaire54@gmail.com>
2023-08-02 00:48:14 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,26 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _pow_recursion - calculate the root of a number.
+ * @x: the numbers root to be found
+ * @y: the number to raised to
+ * Return: always success
  */
-int main(void)
-{
-int r;
 
-r = _pow_recursion(1, 10);
-printf("%d\n", r);
-r = _pow_recursion(1024, 0);
-printf("%d\n", r);
-r = _pow_recursion(2, 16);
-printf("%d\n", r);
-r = _pow_recursion(5, 2);
-printf("%d\n", r);
-r = _pow_recursion(5, -2);
-printf("%d\n", r);
-r = _pow_recursion(-5, 3);
-printf("%d\n", r);
-return (0);
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+
+if (y == 0)
+return (1);
+
+return (x * _pow_recursion(x, y - 1));
 }
